172c3ad59c980dc7156d0246df583023435cc6cf
Reio Piller <reio@heathmont.net>
2013-09-21 17:51:34 +0300

    dev

---
+++ Assets/Manifests/InventorySlotManifest.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class InventorySlotManifest : PositionalManifest {
+
+	public ModuleManifest module;
+	public int count;
+
+	public InventorySlotManifest() {
+	}
+
+	public InventorySlotManifest(MonoBehaviour behaviour) {
+
+	}
+
+}
--- Assets/Resources/Modules/Stats/InternalModuleStats.cs
+++ Assets/Resources/Modules/Stats/InternalModuleStats.cs
@@ -2,5 +2,7 @@ using UnityEngine;
 using System;
 
 public class InternalModuleStats : ModuleStats {
-
+	public InternalModuleStats() {
+		mass = .5f;
+	}
 }
